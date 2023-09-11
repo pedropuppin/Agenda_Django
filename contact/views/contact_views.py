e480803686b591b093e86ea3157a72cf1ad5b5e3
Pedro Puppin <puppin.pedro@gmail.com>
2023-09-10 22:38:18 -0300

    novas partials para o header e o head. Mudanças no titulo do site

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -15,6 +15,7 @@ def index(request):
     
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos -'
     }
     
     return render(
@@ -42,8 +43,11 @@ def contact(request, contact_id):
         show=True,
     )
     
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
     
     return render(
