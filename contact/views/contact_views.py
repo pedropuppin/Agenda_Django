1c1ef167c2af61492f8fd1f9a350142839f0e019
Pedro Puppin <puppin.pedro@gmail.com>
2023-09-12 10:02:41 -0300

    adicionei o campo de pesquisa e a lógica dos filtros

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,12 @@
 # esse arquivo só funciona dessa forma pq no __init__.py a gente tá importando 
 # tudo que tem dentro dele
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.db.models import Q # permite usar 'or' nos filtros da pesquisa
+
+
 
 # Create your views here.
 
@@ -53,5 +56,36 @@ def contact(request, contact_id):
     return render(
         request, 
         'contact/contact.html',
+        context,
+    )
+
+def search(request):
+    # o q é o nome do form lá no _header
+    search_value = request.GET.get('q', '').strip() # GET retorna um QueryDict
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    # field lookups - https://docs.djangoproject.com/en/4.2/ref/models/querysets/#field-lookupshttps://docs.djangoproject.com/en/4.2/ref/models/querysets/#field-lookups
+    contacts = Contact.objects.filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    # print(contacts.query) # mostra a query que está sendo executada
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search -',
+        'search_value': search_value,
+    }
+    
+    return render(
+        request, 
+        'contact/index.html',
         context,
     )
