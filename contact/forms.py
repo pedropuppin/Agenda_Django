048ac3a366221722d7941d5ad6a739e81e8063de
Pedro Puppin <puppin.pedro@gmail.com>
2023-09-13 11:46:20 -0300

    adicionado o botão de delete e upload de imagens

--- contact/forms.py
+++ contact/forms.py
@@ -2,12 +2,14 @@ from . import models
 from django import forms
 from django.core.exceptions import ValidationError
 
-# criando um formulário a partir de um modelo
+# criando um formulário a partir de um modelo(forms.ModelForm)
 class ContactForm(forms.ModelForm):
+    # os widgets são os elementos que serão renderizados na tela
     first_name = forms.CharField(
         widget = forms.TextInput(
+            # attrs são os atributos do elemento
             attrs = {
-                'class': 'form-control',
+                'class': 'minha-nova-classe',
                 'placeholder': 'Digite seu nome',
             }
         ),
@@ -15,17 +17,25 @@ class ContactForm(forms.ModelForm):
         # help_text = 'Digite seu nome',
     )
     
+    picture = forms.ImageField( # cria o campo novamente
+        widget = forms.FileInput( # fala que é pra ser um campo de arquivo
+            attrs = {
+                'accept': 'image/*', # aceita qualquer imagem que for enviada
+            } 
+        )
+    )
+    
     class Meta:
-        model = models.Contact
-        fields = (
+        model = models.Contact # informa qual o model em que o form é baseado
+        fields = ( # informa quais campos serão utilizados no formulário
             'first_name', 
             'last_name', 
             'phone',
             'email',
             'description',
             'category',
+            'picture',
         )
-        
         
     # uma forma de validar campos para quando vc precisa de mais de uma validação de campos diferentes
     def clean(self):
@@ -44,6 +54,7 @@ class ContactForm(forms.ModelForm):
             self.add_error('last_name', msg)
                 
         return super().clean()
+
 
     # Outra forma de falidação de um campo expecífico
     def clean_first_name(self):
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -7,10 +7,14 @@ def create(request):
     # pega a entrada do form
     # search = request.POST.get('first_name')
     
+    # manda o tipo de action do formulário para o template
     form_action = reverse('contact:create')
     
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        # chama o form de forms.py com os dados do template. O primeiro parametro é o data
+        # e o segundo é o request 
+        form = ContactForm(request.POST, request.FILES) 
+        
         # formulário que foi postado
         context = {
             'form': form,
@@ -24,14 +28,16 @@ def create(request):
             # contact.save()
             contact = form.save()
             return redirect('contact:update', contact_id=contact.id)
-            
+        # else:
+        #     print(form.errors)
+        
         return render(
             request, 
             'contact/create.html',
             context,
         )
 
-    #formulário vazio
+    #formulário vazio (GET)
     context = {
         'form': ContactForm(),
         'form_action': form_action,
@@ -43,6 +49,7 @@ def create(request):
         context,
     )
 
+
 def update(request, contact_id):
     # pega a entrada do form
     # search = request.POST.get('first_name')
@@ -53,7 +60,7 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id, ))
     
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
         # formulário que foi postado
         context = {
             'form': form,
@@ -85,3 +92,32 @@ def update(request, contact_id):
         'contact/create.html',
         context,
     )        
+
+
+def delete(request, contact_id):
+
+    contact = get_object_or_404(
+        Contact, 
+        id=contact_id, 
+        show=True
+    )
+    
+    # request.POST é um dict vindo do form delete do HTML. Estamos usando o .get()
+    # para lidar com os dados do dict, falando pra pegar o valor de 'confirmation'
+    # e se nenhum valor for encontrado retornamos 'no' por default.
+    confirmation = request.POST.get('confirmation', 'no')
+    # print('confirma', confirmation) // confirma no
+
+    if confirmation == 'yes':
+        # print('confirma', confirmation) // confirma yes
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact, # precisa passar o contexto p o template
+            'confirmation': confirmation, # passa o valor de confirmation para o template
+        }
+    )
